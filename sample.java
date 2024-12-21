9e1e6ba4e957bd800ae4aa798f7a3719637c61a1
DebashishMahato198 <devmahato198@gmail.com>
2024-12-21 16:05:28 +0530

    third change

--- sample.java
+++ sample.java
@@ -2,5 +2,7 @@ public class sample
 {
     public static void main(String[] args) {
         System.out.println("My first Program in git");
+        System.out.println("second change");
+        System.out.println("hello");
     }
 }
